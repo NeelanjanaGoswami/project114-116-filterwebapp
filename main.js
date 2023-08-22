51521e92af8bc197734f9b23d4a1db8367ef736c
NeelanjanaGoswami <87737175+NeelanjanaGoswami@users.noreply.github.com>
2023-08-22 12:19:27 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+
+function preload(){
+
+}
+
+function draw(){
+
+}
+
+function onClick(){
+    save('myFilterImage.png');  
+}
